c1c302ab04c5176c269251b5be57c8cd7c9683d7
Ben12344 <btirabos@our.ecu.edu.au>
2020-03-23 03:24:54 +0000

    Project

---
+++ portfolio/guessage.sh
@@ -0,0 +1,45 @@
+#/bin/bash
+
+
+
+echo "Wellcome to age guess game!"
+
+while true; do
+
+read -p "Press p to play new round or Press e to finish : " player
+
+if [ "$player" = "p" ]; then
+
+    currentage=$((( RANDOM % 100 ) + 1 ))
+
+    start="$(date -u +%s)" 
+
+    echo "Guess the age between 0-100"
+    
+    while true; do
+
+    read -p 'Enter age : ' guessage
+
+    if [[ "$guessage" =~ ^[0-9]+$ ]]; then
+
+        [ "$guessage" = "$currentage" ] && stop="$(date -u +%s)" && timedif="$(($stop-$start))" && echo "Correct! the current age is $currentage and you completed in $timedif seconds"  &&  break
+        [ "$guessage" -gt "$currentage" ] && echo "Too high try again!"
+        [ "$guessage" -lt "$currentage" ] && echo "Too low try again!"
+
+   else 
+        echo "No float/deciminals and/or strings/characters and/or no value"
+    fi
+
+    done
+
+elif [ "$player" = "e" ]; then
+    echo "Thanks for playing"
+    break
+
+else 
+    echo "Invaild input"
+
+fi
+
+
+done 
