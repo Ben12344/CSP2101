2e8331ba0edb5f6d9285150fd91b548d828b99f2
Ben12344 <btirabos@our.ecu.edu.au>
2020-03-30 01:25:36 +0000

    new work

--- portfolio/guessage.sh
+++ portfolio/guessage.sh
@@ -1,41 +1,56 @@
 #/bin/bash
 
+#Name: Ben Tirabosco
+#ID: 10393081
 
 
+#Menu to guessage
 echo "Wellcome to age guess game!"
 
+#Keep menu after or exit when user finishes
 while true; do
 
     read -p "Press p to play new round or Press e to finish : " player
 
     if [[ "$player" = "p" ]]; then
 
+        #Generate Random Age
         currentage=$((( RANDOM % 100 ) + 1 ))
 
-        start="$(date -u +%s)" 
-
         echo "Guess the age between 0-100"
+
+        #Start Time
+        start="$(date -u +%s)" 
         
+
+        #Keep guess age game unitl user guess age = age from radnom generator
         while true; do
 
             read -p 'Enter age : ' guessage
 
+            #Check if user input is an integer 
             if [[ "$guessage" =~ ^[0-9]+$ ]]; then
 
+                # guess age equal to age from random generator and finish game 
                 [ "$guessage" = "$currentage" ] && stop="$(date -u +%s)" && timedif="$(($stop-$start))" && echo "Correct! the current age is $currentage and you completed in $timedif seconds"  &&  break
+                #guess age greater than age from random generator
                 [ "$guessage" -gt "$currentage" ] && echo "Too high try again!"
+                #guess age less than age from random generator
                 [ "$guessage" -lt "$currentage" ] && echo "Too low try again!"
 
+            #User input is not integer 
             else 
                 echo "No float/deciminals and strings/characters "
             fi
 
         done
 
+    #player finsih game
     elif [[ "$player" = "e" ]]; then
         echo "Thanks for playing"
         break
 
+    #input invaild 
     else 
         echo "Invaild input"
 
---
+++ workshop/week5/WS5task1.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+echo -e "Documents/csp2101/workshop/week4/workshop4task1.sh"< userinput.txt
+
